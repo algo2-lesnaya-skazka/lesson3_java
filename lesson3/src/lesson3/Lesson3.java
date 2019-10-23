00ceec265df41fa97c96233db1e2d2e4a9061d68
Yarik Bespalov <bespalovyarik@yandex.ru>
2019-10-23 18:51:41 +0300

    Add lesson3_java files

---
+++ lesson3/src/lesson3/Lesson3.java
@@ -0,0 +1,12 @@
+package lesson3;
+
+public class Lesson3 {
+	
+public static void main(String[] args) {
+	System.out.println("*    * ***** *      *        ****");
+    System.out.println("*    * *     *      *       *    *");
+    System.out.println("****** ***** *      *       *    *");
+    System.out.println("*    * *     *      *       *    *");
+    System.out.println("*    * ***** *****  *****    ****");
+}
+}
